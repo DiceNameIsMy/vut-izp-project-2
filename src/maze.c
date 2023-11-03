5951d40ed08328cc61185708d98d68ff5b526a06
Nurdaulet Turar <nurdaulet@byallmeans.work>
2023-11-03 15:01:30 +0100

    ref: main file

--- src/maze.c
+++ src/maze.c
@@ -13,56 +13,69 @@ static const char HELP_TEXT[] = ""
                                 "  --rpath\n"
                                 "  --lpath\n"
                                 "  --shortest\n";
-static const char INVALID_ARGS_ERROR[] =
+static const char INVALID_ARGS_AMOUNT_ERROR[] =
     "Invalid amount of arguments. See --help for more information.\n";
+static const char UNKNOWN_STATEGY_ERROR[] =
+    "Unknown strategy `%s`. See --help for more information.\n";
+
+void test_maze(char *filename) {
+    FILE *maze_file = fopen(filename, "r");
+    if (maze_file == NULL) {
+        printf("Invalid\n");
+        return;
+    }
+
+    Map map;
+    int r = construct_map(&map, maze_file);
+    if (r != 0) {
+        printf("Invalid\n");
+        return;
+    }
+
+    fclose(maze_file);
+    destruct_map(&map);
+
+    printf("Valid\n");
+}
 
 int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        printf(HELP_TEXT);
+        return 0;
+    }
+    if (argc > 3) {
+        fprintf(stderr, INVALID_ARGS_AMOUNT_ERROR);
+        return 1;
+    }
+
     char *flag = NULL;
     char *maze_filename = NULL;
 
     for (int i = 1; i < argc; i++) {
         char *argument = argv[i];
-        bool is_flag = strncmp(argument, "--", 2) == 0;
 
-        if (is_flag) {
+        if (strcmp(argument, "--help") == 0) {
+            printf(HELP_TEXT);
+            return 0;
+        } else if (strncmp(argument, "--", 2) == 0) {
             if (flag != NULL) {
-                fprintf(stderr, INVALID_ARGS_ERROR);
+                fprintf(stderr, INVALID_ARGS_AMOUNT_ERROR);
                 return 1;
             }
             flag = argument;
         } else {
             if (maze_filename != NULL) {
-                fprintf(stderr, INVALID_ARGS_ERROR);
+                fprintf(stderr, INVALID_ARGS_AMOUNT_ERROR);
                 return 1;
             }
             maze_filename = argument;
         }
     }
 
-    bool no_args = (flag == NULL && maze_filename == NULL);
-    if (no_args || (strcmp(flag, "--help") == 0)) {
-        printf(HELP_TEXT);
-        return 0;
-    }
-
     if (strcmp(flag, "--test") == 0) {
-        FILE *maze_file = fopen(maze_filename, "r");
-        if (maze_file == NULL) {
-            printf("Invalid\n");
-            return 0;
-        }
-
-        Map map;
-        int r = construct_map(&map, maze_file);
-        if (r != 0) {
-            printf("Invalid\n");
-            return 0;
-        }
-
-        fclose(maze_file);
-        destruct_map(&map);
-
-        printf("Valid\n");
+        test_maze(maze_filename);
+    } else {
+        fprintf(stderr, UNKNOWN_STATEGY_ERROR, flag);
     }
 
     return 0;
