49e7866566cffa17f2b7dc4198e4413d00541227
Nurdaulet Turar <nurdaulet@byallmeans.work>
2023-11-12 15:42:00 +0100

    docs: FIx misleading documentatin

--- src/map.h
+++ src/map.h
@@ -9,17 +9,18 @@ typedef enum strategy { RIGHT_HAND = 0, LEFT_HAND = 1, SHORTEST = 2 } Strategy;
 
 /* ## Represents a map of the maze.
 
-cells - each char of the cells accessor vector represents
-        a cell with 3 passages in the maze.
+cells - each char represents a cell with 3 passages in the maze.
 
-Third rightmost bit (0b100) - border on the left
-Second rightmost bit (0b010) - border on the right
-Rightmost bit (0b001) - border above or below. It depends on the sum of the
+Cell has 3 passages that can have borders that prevent moving through them. It
+is represented by an 8 bit integer. 3 rightmost bits are used. 0 specifies the
+absence of a border on the side, and 1 its presence.
+
+First bit (0b001) -> border on the left
+Second bit (0b010) -> border on the right
+Third bit (0b10) -> border above or below depending on the sum of the
 coordinates:
-    - odd: passage is above
-    - even: passage is below
-
-0 specifies the absence of the border on the side, and 1 its presence.
+    - odd (1 + 2): passage is above
+    - even (1 + 1): passage is below
 
 This structure is the part of the project requirements. Do not change.
 */
