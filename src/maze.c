5a41d4e5f6e311103acdcedba80ccee62c636505
Nurdaulet Turar <nurdaulet@byallmeans.work>
2023-11-26 17:05:32 +0100

    ref: Reorder functions

--- src/maze.c
+++ src/maze.c
@@ -25,7 +25,15 @@ typedef enum strategy { RIGHT_HAND = 0, LEFT_HAND = 1, SHORTEST = 2 } Strategy;
 
 typedef enum border { RIGHT, LEFT, UP, DOWN, BORDER_COUNT } Border;
 
-typedef void ( *on_step_func_t )( int r, int c );
+/*
+For displaying the border in log message as a word instead of a number
+*/
+char *border_str[ BORDER_COUNT ] = {
+    [RIGHT] = "RIGHT",
+    [LEFT] = "LEFT",
+    [UP] = "UP",
+    [DOWN] = "DOWN",
+};
 
 typedef struct position {
     int row;
@@ -37,8 +45,8 @@ typedef struct position {
 cells - each char represents a cell with 3 passages in the maze.
 
 Cell has 3 passages that can have borders that prevent moving through them. It
-is represented by an 8 bit integer. 3 rightmost bits are used. 0 specifies the
-absence of a border on the side, and 1 its presence.
+is represented by an 8 bit integer. 3 rightmost bits are used. Value of 0
+specifies the absence of a border on the passage, and 1 its presence.
 
 First bit (0b001) -> border on the left
 Second bit (0b010) -> border on the right
@@ -46,8 +54,6 @@ Third bit (0b10) -> border above or below depending on the sum of the
 coordinates:
     - odd (1 + 2): passage is above
     - even (1 + 1): passage is below
-
-This structure is the part of the project requirements. Do not change.
 */
 typedef struct map {
     int rows;
@@ -66,6 +72,11 @@ Get the border to start crossing on the first step
 */
 Border start_border( Map *map, int r, int c, int leftright );
 
+/*
+Function to invoke on each step taken to get out of the maze
+*/
+typedef void ( *on_step_func_t )( int r, int c );
+
 void solve_maze( Map *map, int r, int c, Strategy strategy,
                  on_step_func_t on_step_func );
 
@@ -76,24 +87,10 @@ HELPER DEFINITIONS
 
 */
 
-char *border_str[ BORDER_COUNT ] = {
-    [RIGHT] = "RIGHT",
-    [LEFT] = "LEFT",
-    [UP] = "UP",
-    [DOWN] = "DOWN",
-};
-
-int to_cell( char ch, int *cell ) {
-    bool valid = ch >= '0' && ch <= '7';
-    if ( !valid )
-        return -1;
-
-    *cell = ch - '0';
-    return 1;
-}
-
 int get_cell_idx( Map *map, int r, int c ) {
-    return ( ( r - 1 ) * map->cols ) + c - 1;
+    int row_skip_cells = ( r - 1 ) * map->cols;
+    int col_skip_cells = c - 1;
+    return row_skip_cells + col_skip_cells;
 }
 
 int get_cell( Map *map, int r, int c ) {
@@ -101,6 +98,10 @@ int get_cell( Map *map, int r, int c ) {
     int val = map->cells[ idx ];
     return val;
 }
+
+bool has_left_border( int cell ) { return ( cell & 0b001 ) == 0b001; }
+bool has_right_border( int cell ) { return ( cell & 0b010 ) == 0b010; }
+bool has_updown_border( int cell ) { return ( cell & 0b100 ) == 0b100; }
 
 bool out_of_maze( Map *m, int r, int c ) {
     return ( r < 1 || r > m->rows || c < 1 || c > m->cols );
@@ -131,6 +132,9 @@ bool moves_out_of_maze( Map *m, int r, int c, Border direction ) {
 
 bool has_passage_above( int r, int c ) { return ( ( ( r + c ) & 1 ) == 0 ); }
 
+Border reverse_direction[ BORDER_COUNT ] = {
+    [RIGHT] = LEFT, [LEFT] = RIGHT, [UP] = DOWN, [DOWN] = UP };
+
 /*
 
 MAP INITIALIZATION & DESTRUCTION
@@ -155,17 +159,23 @@ int read_map_size( Map *map, FILE *file ) {
     return 0;
 }
 
-typedef enum processCharResult { OK, BAD_MAP, BAD_CELL } ProcessCharResult;
+int to_cell( char ch, int *cell ) {
+    bool is_allowed_number = ch >= '0' && ch <= '7';
+    if ( !is_allowed_number )
+        return -1;
 
-ProcessCharResult process_char( Map *map, char ch, int *row_ptr,
-                                int *col_ptr ) {
+    *cell = ch - '0';
+    return 1;
+}
+
+int process_char( Map *map, char ch, int *row_ptr, int *col_ptr ) {
     if ( ch == ' ' ) {
-        return OK;
+        return 0;
     }
     if ( ch == '\n' ) {
         bool all_row_columns_set = ( *col_ptr - 1 ) == map->cols;
         if ( !all_row_columns_set )
-            return BAD_MAP;
+            return -1;
 
         loginfo( "moving to the next line(row) %i->%i", *row_ptr,
                  *row_ptr + 1 );
@@ -173,12 +183,12 @@ ProcessCharResult process_char( Map *map, char ch, int *row_ptr,
         ( *row_ptr )++;
         *col_ptr = 1;
 
-        return OK;
+        return 0;
     }
 
     int cell;
     if ( to_cell( ch, &cell ) == -1 )
-        return BAD_CELL;
+        return -1;
 
     int cell_idx = get_cell_idx( map, *row_ptr, *col_ptr );
 
@@ -188,7 +198,7 @@ ProcessCharResult process_char( Map *map, char ch, int *row_ptr,
     map->cells[ cell_idx ] = cell;
     ( *col_ptr )++;
 
-    return OK;
+    return 0;
 }
 
 int read_map_cells( Map *map, FILE *file ) {
@@ -197,20 +207,17 @@ int read_map_cells( Map *map, FILE *file ) {
 
     char c;
     while ( ( c = fgetc( file ) ) != EOF ) {
-        ProcessCharResult r = process_char( map, c, &row, &column );
-        if ( r == BAD_MAP || r == BAD_CELL )
+        if ( process_char( map, c, &row, &column ) == -1 )
             return -1;
     }
+    // At the end of the file there should be a '\n' that moves the row pointer
+    // to the next row. That's why comparing with `map->rows + 1`
     bool all_rows_processed = row == ( map->rows + 1 );
     if ( !all_rows_processed ) {
         return -1;
     }
     return 0;
 }
-
-bool has_left_border( int cell ) { return ( cell & 0b001 ) == 0b001; }
-bool has_right_border( int cell ) { return ( cell & 0b010 ) == 0b010; }
-bool has_updown_border( int cell ) { return ( cell & 0b100 ) == 0b100; }
 
 bool check_right_border( Map *map, int cell, int r, int c ) {
     if ( moves_out_of_maze( map, r, c, RIGHT ) )
@@ -244,14 +251,12 @@ bool check_down_border( Map *map, int cell, int r, int c ) {
 bool check_cell_valid( Map *map, int r, int c ) {
     int cell = get_cell( map, r, c );
 
-    bool right_border_valid = check_right_border( map, cell, r, c );
-    bool down_border_valid = check_down_border( map, cell, r, c );
+    bool right_passage_valid = check_right_border( map, cell, r, c );
+    bool down_passage_valid = check_down_border( map, cell, r, c );
 
-    return right_border_valid && down_border_valid;
+    return right_passage_valid && down_passage_valid;
 }
 
-/* check vailidy of every cell's right & down border starting from top left
- */
 bool is_map_valid( Map *map ) {
     for ( int r = 1; r <= map->rows; r++ ) {
         for ( int c = 1; c <= map->cols; c++ ) {
@@ -270,10 +275,8 @@ void destruct_map( Map *map ) {
 
 Map *load_map( FILE *file ) {
     Map *map = malloc( sizeof( Map ) );
-    if ( map == NULL ) {
-        free( map );
+    if ( map == NULL )
         return NULL;
-    }
 
     if ( read_map_size( map, file ) == -1 ) {
         free( map );
@@ -307,74 +310,6 @@ STARTING POINT RESOLVEMENT
 
 */
 
-/* Resolve to what passage to look at next
-- first accessor is a strategy: LEFT_HAND / RIGHT_HAND
-- second accessor: Border where from player came
-- third accessor: Whether player's current cell has a passage UP
- */
-Border next_step_ruleset[ 2 ][ BORDER_COUNT ][ 2 ] = {
-    [RIGHT_HAND] = { [RIGHT] = { LEFT, UP },
-                     [LEFT] = { DOWN, RIGHT },
-                     [UP] = { LEFT, LEFT },
-                     [DOWN] = { RIGHT, RIGHT } },
-    [LEFT_HAND] = { [RIGHT] = { DOWN, LEFT },
-                    [LEFT] = { RIGHT, UP },
-                    [UP] = { RIGHT, RIGHT },
-                    [DOWN] = { LEFT, LEFT } } };
-
-Border entered_maze_from( Map *map, int r, int c ) {
-    if ( c == 1 )
-        return LEFT;
-    else if ( c == map->cols )
-        return RIGHT;
-    else if ( r == 1 )
-        return UP;
-    else if ( r == map->rows )
-        return DOWN;
-
-    return -1;
-}
-
-Border resolve_direction( Map *m, int r, int c, Strategy leftright,
-                          Border came_from ) {
-    bool can_go_up = has_passage_above( r, c );
-
-    Border dir = next_step_ruleset[ leftright ][ came_from ][ can_go_up ];
-
-    if ( isborder( m, r, c, dir ) ) {
-        loginfo( "can not go %s", border_str[ dir ] );
-        return resolve_direction( m, r, c, leftright, dir );
-    }
-    return dir;
-}
-
-Border reverse_direction[ BORDER_COUNT ] = {
-    [RIGHT] = LEFT, [LEFT] = RIGHT, [UP] = DOWN, [DOWN] = UP };
-
-Border start_border( Map *map, int r, int c, int leftright ) {
-    if ( leftright == SHORTEST ) {
-        loginfo( "invalid leftright value %i", leftright );
-        return -1;
-    }
-
-    Border entered_from = entered_maze_from( map, r, c );
-    if ( (int)entered_from == -1 ) {
-        loginfo( "not entering the maze from its borders. Maze size: %ix%i", r,
-                 c );
-        return -1;
-    }
-    loginfo( "entered maze from %s", border_str[ entered_from ] );
-
-    return resolve_direction( map, r, c, leftright, entered_from );
-}
-
-/*
-
-NOT GROUPED YET
----------------------------------------------------------------------
-
-*/
-
 bool isborder( Map *map, int r, int c, Border border ) {
     int cell = get_cell( map, r, c );
 
@@ -395,37 +330,66 @@ bool isborder( Map *map, int r, int c, Border border ) {
     return true;
 }
 
-void solve_maze( Map *map, int r, int c, Strategy strategy,
-                 on_step_func_t on_step_func ) {
-    if ( strategy == SHORTEST ) {
-        return;
+/* Resolve to what passage to look at next
+- first accessor is a strategy: LEFT_HAND / RIGHT_HAND
+- second accessor: Border where from player came
+- third accessor: Whether player's current cell has a passage UP
+ */
+Border next_step_ruleset[ 2 ][ BORDER_COUNT ][ 2 ] = {
+    [RIGHT_HAND] = { [RIGHT] = { LEFT, UP },
+                     [LEFT] = { DOWN, RIGHT },
+                     [UP] = { LEFT, LEFT },
+                     [DOWN] = { RIGHT, RIGHT } },
+    [LEFT_HAND] = { [RIGHT] = { DOWN, LEFT },
+                    [LEFT] = { RIGHT, UP },
+                    [UP] = { RIGHT, RIGHT },
+                    [DOWN] = { LEFT, LEFT } } };
+
+/*
+Resolve where to move next. If there is a border on the passage, try the next
+best option.
+*/
+Border resolve_direction( Map *m, int r, int c, Strategy leftright,
+                          Border came_from ) {
+    bool can_go_up = has_passage_above( r, c );
+
+    Border dir = next_step_ruleset[ leftright ][ came_from ][ can_go_up ];
+
+    if ( isborder( m, r, c, dir ) ) {
+        loginfo( "can not go %s", border_str[ dir ] );
+        return resolve_direction( m, r, c, leftright, dir );
+    }
+    return dir;
+}
+
+Border entered_maze_from( Map *map, int r, int c ) {
+    if ( c == 1 )
+        return LEFT;
+    else if ( c == map->cols )
+        return RIGHT;
+    else if ( r == 1 )
+        return UP;
+    else if ( r == map->rows )
+        return DOWN;
+
+    return -1;
+}
+
+Border start_border( Map *map, int r, int c, int leftright ) {
+    if ( leftright == SHORTEST ) {
+        loginfo( "invalid leftright value %i", leftright );
+        return -1;
     }
 
-    Border direction = start_border( map, r, c, strategy );
-    if ( (int)direction == -1 ) {
-        loginfo( "failed to get the starting border with entering points %ix%i",
-                 r, c );
-        return;
+    Border entered_from = entered_maze_from( map, r, c );
+    if ( (int)entered_from == -1 ) {
+        loginfo( "not entering the maze from its borders. Maze size: %ix%i", r,
+                 c );
+        return -1;
     }
-    loginfo( "starting direction is %s", border_str[ direction ] );
+    loginfo( "entered maze from %s", border_str[ entered_from ] );
 
-    int steps = 0;
-    while ( true ) {
-        // Act
-        on_step_func( r, c );
-
-        // Move
-        r = move_r( r, direction );
-        c = move_c( c, direction );
-        if ( out_of_maze( map, r, c ) ) {
-            loginfo( "exit from maze was found in %i steps", steps );
-            return;
-        }
-
-        Border came_from = reverse_direction[ direction ];
-        direction = resolve_direction( map, r, c, strategy, came_from );
-        steps++;
-    }
+    return resolve_direction( map, r, c, leftright, entered_from );
 }
 
 /*
@@ -441,6 +405,9 @@ int try_test_maze( char *option, char *filename );
 
 int try_solve_maze( char *option, char *row, char *column, char *filename );
 
+void solve_maze( Map *map, int r, int c, Strategy strategy,
+                 on_step_func_t on_step_func );
+
 static const char HELP_TEXT[] =
     ""
     "Usage: ./maze [options] file...\n"
@@ -449,7 +416,8 @@ static const char HELP_TEXT[] =
     "  --test                     Test if given file is a valid maze.\n"
     "  --rpath <row> <column>     Solve maze using the right hand rule.\n"
     "  --lpath <row> <column>     Solve maze using the left hand rule.\n"
-    "  --shortest <row> <column>  Solve maze by finding the shortest path to "
+    "  --shortest <row> <column>  Solve maze by finding the shortest path "
+    "to "
     "the exit.\n";
 static const char INVALID_ARGS_ERROR[] =
     "Invalid argument `%s`. Try `maze --help` for more information.\n";
@@ -580,4 +548,37 @@ int try_solve_maze( char *option, char *row, char *column, char *filename ) {
 
     destruct_map( map );
     return 0;
+}
+
+void solve_maze( Map *map, int r, int c, Strategy strategy,
+                 on_step_func_t on_step_func ) {
+    if ( strategy == SHORTEST ) {
+        return;
+    }
+
+    Border direction = start_border( map, r, c, strategy );
+    if ( (int)direction == -1 ) {
+        loginfo( "failed to get the starting border with entering points %ix%i",
+                 r, c );
+        return;
+    }
+    loginfo( "starting direction is %s", border_str[ direction ] );
+
+    int steps = 0;
+    while ( true ) {
+        // Act
+        on_step_func( r, c );
+
+        // Move
+        r = move_r( r, direction );
+        c = move_c( c, direction );
+        if ( out_of_maze( map, r, c ) ) {
+            loginfo( "exit from maze was found in %i steps", steps );
+            return;
+        }
+
+        Border came_from = reverse_direction[ direction ];
+        direction = resolve_direction( map, r, c, strategy, came_from );
+        steps++;
+    }
 }
